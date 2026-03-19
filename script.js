fa63a69ff6ba7bfe1c77a5f5b1fcaf5ea7523785
skfurnitures <priyanshusakpal11@gmail.com>
2026-03-19 08:53:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
+function goGallery() {
+  window.location.href = "gallery.html";
+}
 
+function call() {
+  window.location.href = "tel:9876543210";
+}
+
+function whatsapp() {
+  window.location.href = "https://wa.me/919876543210";
+}
+
+function filter(category) {
+  let images = document.querySelectorAll(".gallery img");
+
+  images.forEach(img => {
+    if (category === "all" || img.classList.contains(category)) {
+      img.style.display = "block";
+    } else {
+      img.style.display = "none";
+    }
+  });
+}
+
+function loadMore() {
+  let gallery = document.getElementById("gallery");
+
+  let img = document.createElement("img");
+  img.src = "https://images.unsplash.com/photo-1595526114035-0d45ed16cfbf";
+
+  gallery.appendChild(img);
+}
